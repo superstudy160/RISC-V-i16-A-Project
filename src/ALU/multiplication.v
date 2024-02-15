da9a5f041907232264cc6e75bc9fe458cc5f4da4
Roman Mishchuk <ge74jat@mytum.de>
2024-02-15 20:28:45 +0200

    Suggestions for the multiplication algorithms and one more structure edit

--- src/ALU/multiplication.v
+++ src/ALU/multiplication.v
@@ -1,3 +1,12 @@
+// Suggestions for this stuff:
+// https://en.wikipedia.org/wiki/Booth%27s_multiplication_algorithm
+// http://vlsiip.com/download/booth.pdf
+// The comparisson of different hardware multipliers:
+// https://en.wikipedia.org/wiki/Binary_multiplier#Hardware_implementation
+// Fast multiplication:
+// https://watermark.silverchair.com/020085_1_online.pdfhttps://pubs.aip.org/aip/acp/article-pdf/doi/10.1063/1.5080898/14172964/020085_1_online.pdf
+// https://ietresearch.onlinelibrary.wiley.com/doi/full/10.1049/iet-cds.2019.0537
+
 module multiplication (
     input [0:15] M1,
     input [0:15] M2
