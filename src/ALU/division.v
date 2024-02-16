b30a9226f6c031542ea826f4f20e05e793ce0ad1
Roman Mishchuk <ge74jat@mytum.de>
2024-02-16 20:05:52 +0200

    ControlledSubtract done

--- src/ALU/division.v
+++ src/ALU/division.v
@@ -1,3 +1,37 @@
+// https://www.geeksforgeeks.org/full-subtractor-in-digital-logic/
+module Subtract(
+    input A, // Minuend
+    input B, // Subtrahend
+    input Bin, // Borrow in
+    output D, // Difference
+    output Bout // Borrow out
+);
+
+assign D = A ^ B ^ Bin;
+assign Bout = (~A & B) | ((~(A ^ B)) & Bin);
+    
+endmodule //Subtract
+
+module ControlledSubtract(
+    input A, // Minuend
+    input B, // Subtrahend
+    input Bin, // Borrow in
+    input Selection, // 0 for subtraction, 1 to skip
+    output D, // Difference
+    output Bout // Borrow out
+);
+
+wire d_buff;
+
+Subtract subtractor(
+    .A(A), .B(B), .Bin(Bin),
+    .D(d_buff), .Bout(Bout)
+);
+
+assign D = Selection ? A : d_buff;
+
+endmodule //ControlledSubtract
+
 module Division #(parameter l=16) (
     input [lv:0] A,
     input [lv:0] B,
