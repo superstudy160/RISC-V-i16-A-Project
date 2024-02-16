c074193a4e44c439dd7d4e9d122491bff4bccde5
Roman Mishchuk <ge74jat@mytum.de>
2024-02-16 14:23:08 +0200

    Addition module split into several

--- src/ALU/addition.v
+++ src/ALU/addition.v
@@ -1,30 +1,60 @@
-module FullAdderSigned16bit (
-    input [15:0] A,
-    input [15:0] B,
+module Adder (
+    input A,
+    input B,
     input Cin,
-    output [15:0] S,
-    output Cout,
-    output Overflow
+    output S,
+    output Cout
 );
-wire [15:0] Sum;
-wire [15:0] Cout_temp;
 
-// First bit
-assign Sum[0] = A[0] ^ B[0] ^ Cin;
-assign Cout_temp[0] = (A[0] & B[0]) | ((A[0] ^ B[0]) & Cin);
+assign S = A ^ B ^ Cin;
+assign Cout = (A & B) | ((A ^ B) & Cin);
+
+endmodule //Adder
+
+module FullAdder #(parameter l = 16) (
+    input [lv:0] A,
+    input [lv:0] B,
+    input Cin,
+    output [lv:0] S,
+    output [lv:0] Cout
+);
+
+parameter lv = l-1;
+
+wire [lv:0] Sum;
+wire [l:0] Cout_temp;
+assign Cout_temp[0] = Cin;
 
 // Remaining bits
 genvar i;
 generate
     //                                     |-> Why you do this?
-    for (i = 1; i <= 15; i = i + 1) begin : gen_adders
-        assign Sum[i] = A[i] ^ B[i] ^ Cout_temp[i-1];
-        assign Cout_temp[i] = (A[i] & B[i]) | ((A[i] ^ B[i]) & Cout_temp[i-1]);
+    for (i = 0; i <= lv; i = i + 1) begin : gen_adders
+        Adder adder(A[i], B[i], Cout_temp[i], Sum[i], Cout_temp[i+1]);
     end
 endgenerate
 
 assign S = Sum;
-assign Cout = Cout_temp[15];
-assign Overflow = Cout_temp[14] ^ Cout_temp[15]; // this code works somehow, at least the possibilities I have tested
+assign Cout[lv:0] = Cout_temp[l:1];
+
+endmodule //FullAdder
+
+module FullAdderSigned #(parameter l = 16) (
+    input [lv:0] A,
+    input [lv:0] B,
+    output [lv:0] S,
+    output Overflow
+);
+
+parameter lv = l-1;
+
+wire [lv:0] Cout;
+
+FullAdder #(l) full_adder (
+    .A(A), .B(B), .Cin(1'b0),
+    .S(S), .Cout(Cout)
+);
+
+assign Overflow = Cout[lv-1] ^ Cout[lv]; // this code works somehow, at least the possibilities I have tested
 
 endmodule
--- src/ALU/addition_tb.v
+++ src/ALU/addition_tb.v
@@ -1,26 +1,23 @@
 `include "./addition.v"
 
 // This was generated using Copilot
-module FullAdderSigned16bit_Testbench #(parameter l=16) ();
+module FullAdderSigned_Testbench #(parameter l=16) ();
 
     parameter lv = l-1;
 
     reg [lv:0] a, b;
     wire [lv:0] sum;
-    wire cout;
     wire overflow;
 
-    FullAdderSigned16bit uut (
+    FullAdderSigned #(l) uut (
         .A(a), 
         .B(b),
-        .Cin(1'b0),
         .S(sum),
-        .Cout(cout),
         .Overflow(overflow)
     );
     
     initial begin
-        $monitor("  a=%b\n  b=%b\nsum=%b, cout=%b, o=%b\n\n", a, b, sum, cout, overflow);
+        $monitor("  a=%b\n  b=%b\nsum=%b, o=%b\n\n", a, b, sum, overflow);
         a = 16'b0000000000000000;
         b = 16'b0000000000000000;
         #10;
