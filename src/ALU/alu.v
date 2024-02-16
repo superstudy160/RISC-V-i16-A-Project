971b757b358203270c3657a5e6b87ffd0b9c8c34
Roman Mishchuk <ge74jat@mytum.de>
2024-02-16 14:30:12 +0200

    ALU interface done

---
+++ src/ALU/alu.v
@@ -0,0 +1,12 @@
+module ALU #(parameter l=16) (
+    input [lv:0] A,
+    input [lv:0] B,
+    input [2:0] Operation,
+    output [lv:0] R,
+)
+
+parameter lv = l-1;
+
+// TODO: implement ALU
+
+endmodule //ALU
