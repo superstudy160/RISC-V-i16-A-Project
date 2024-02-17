d87398e2fcfa8fe41629ee63e5fd7463802bab11
Roman Mishchuk <ge74jat@mytum.de>
2024-02-17 16:11:49 +0200

    First approach to division

--- src/ALU/division.v
+++ src/ALU/division.v
@@ -12,6 +12,7 @@ assign Bout = (~A & B) | ((~(A ^ B)) & Bin);
     
 endmodule //Subtract
 
+// A - B
 module ControlledSubtract(
     input A, // Minuend
     input B, // Subtrahend
@@ -36,12 +37,42 @@ module Division #(parameter l=16) (
     input [lv:0] A,
     input [lv:0] B,
     output [lv:0] Quotient,
-    output [lv:0] Remainder,
+    output [l:0] Remainder,
     output DivByZero
 );
 
 parameter lv = l-1;
 
-// TODO: Implement the division
+wire [l:0] Difference [lv:0];
+wire [l:0] Borrow [lv:0];
+
+generate
+genvar i, j;
+
+for (i = 0; i < l; i = i + 1) begin // <- depth of the division
+    //            |-> We are doing one more cycle to put down next bit
+    for(j = 0; j <= l; j = j + 1) begin
+        ControlledSubtract controlled_subtractor(
+            .A(
+                (j == 0) ? A[lv-i] :  // Putting down the next bit
+                (i > 0) ? Difference[i-1][j-1] : // Propagating the previous bits
+                1'b0 // Zero padding otheriwse
+            ),
+            .B(j < l ? B[j] : 1'b0), // Last bit without substractend (needed for borrow)
+            .Bin(j == 0 ? 1'b0 : Borrow[i][j-1]), // First borrow is always 0
+            .Selection(Borrow[i][l]), // Don't need to borrow => do subtraction
+
+            .D(Difference[i][j]),
+            .Bout(Borrow[i][j])
+        );
+    end
+end
+
+for (i = 0; i < l; i = i + 1)
+    assign Quotient[i] = ~Borrow[i][lv-i];
+
+assign Remainder = Difference[lv];
+
+endgenerate
     
 endmodule //Division
