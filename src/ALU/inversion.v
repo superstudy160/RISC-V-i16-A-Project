2944cffc50cd43befe17e555f85c24738ca2f7dc
Roman Mishchuk <ge74jat@mytum.de>
2024-02-25 16:38:34 +0200

    Invertor overflow is now being handlelt

--- src/ALU/inversion.v
+++ src/ALU/inversion.v
@@ -10,19 +10,20 @@ Where M is all ones.
 */
 module Inverter #(parameter l=16) (
 	input [lv:0] A,
-	output [lv:0] R
+	output [lv:0] R,
+	output Overflow
 );
 
 parameter lv = l-1;
 
-wire [lv:0] ignore;
-FullAdder #(l) full_adder(
-	.A(~A[lv:0]),
-	.B({l{1'b0}}),
-	.Cin(1'b1),
+wire ignore;
+FullAdderFlags #(l) full_adder_flags(
+	.A(~A),
+	.B({{l-1{1'b0}}, 1'b1}),
 
-	.S(R[lv:0]),
-	.Cout(ignore)
+	.S(R),
+	.Overflow(Overflow), // This would correspond to not changing the sign of A
+	.Carry(ignore)
 );
 
 endmodule
@@ -37,8 +38,10 @@ module AbsoluteValue #(parameter l=16) (
 parameter lv = l-1;
 
 wire [lv:0] inv;
+wire ignore;
 Inverter #(l) inverter(
-	.A(A), .R(inv)
+	.A(A), .R(inv),
+	.Overflow(ignore)
 );
 
 assign R = A[lv] ? inv : A;
@@ -49,16 +52,20 @@ endmodule
 module GiveSign #(parameter l=16) (
 	input Sign, // 1 for negative, 0 for positive
 	input [lv:0] A,
-	output [lv:0] R
+	output [lv:0] R,
+	output Overflow
 );
 
 parameter lv = l-1;
 
 wire [lv:0] inv;
+wire ignore;
 Inverter #(l) inverter(
-	.A(A), .R(inv)
+	.A(A), .R(inv),
+	.Overflow(ignore)
 );
 
 assign R = Sign ? inv : A;
+assign Overflow = !Sign & A == {1'b1, {l-1{1'b0}}}; // E.g., if l=4, there is no way to represenet +8 in 2's complement
 
 endmodule
--- src/ALU/inversion_tb.v
+++ src/ALU/inversion_tb.v
@@ -5,22 +5,65 @@ module Inverter_Testbench ();
 
 parameter l = 4;
 reg signed [l-1:0] A;
-wire signed [l-1:0] R;
 
+wire signed [l-1:0] R_inv;
+wire Overflow_inv;
 Inverter #(l) inverter(
-    .A(A), .R(R)
+    .A(A), .R(R_inv),
+    .Overflow(Overflow_inv)
+);
+
+wire [l-1:0] R_abs;
+AbsoluteValue #(l) absolute_value(
+    .A(A), .R(R_abs)
+);
+
+reg Sign;
+reg [l-1:0] A_sign;
+wire signed [l-1:0] R_sign;
+wire Overflow_sign;
+GiveSign #(l) give_sign(
+    .Sign(Sign),
+    .A(A_sign), .R(R_sign),
+    .Overflow(Overflow_sign)
 );
 
 initial begin
-    $monitor("A=%d\nR=%d\n", A, R);
-    #10;
+    $display("Testing inverter:");
+    $monitor("A=%d\nR=%d  O=%b\n", A, R_inv, Overflow_inv);
     A = -4'sd1;
     #10;
-    A = -4'sd8; // TODO: for this testcase the output is 0, and I am not sure what is it actually supposed to be
+    A = -4'sd8;
     #10;
     A = 4'sd7;
     #10;
     A = 4'sd5;
+    #10;
+
+    $display("Testing absolute value:");
+    $monitor("A=%d R=%d\n", A, R_abs);
+    A = -4'sd1;
+    #10;
+    A = -4'sd8;
+    #10;
+    A = 4'sd7;
+    #10;
+    A = -4'sd5;
+    #10;
+
+    $display("Testing sign setting:");
+    $monitor("Sign=%d\nA=%d R=%d  O=%b\n", Sign, A_sign, R_sign, Overflow_sign);
+    Sign = 1'b0;
+    A_sign = 4'd5;
+    #10;
+    Sign = 1'b0;
+    A_sign = 4'd8;
+    #10;
+    Sign = 1'b1;
+    A_sign = 4'd8;
+    #10;
+    Sign = 1'b1;
+    A_sign = 4'd5;
     #10;
 end
 
