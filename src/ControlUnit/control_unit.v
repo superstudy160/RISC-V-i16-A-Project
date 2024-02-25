3aa88fe0d86b9405e623f00b936ad3d0a49f3bd8
Roman Mishchuk <ge74jat@mytum.de>
2024-02-25 17:02:21 +0200

    Working control unit

--- src/ControlUnit/control_unit.v
+++ src/ControlUnit/control_unit.v
@@ -1,1 +1,47 @@
-Hello!
+module ContolUnit #(parameter l = 16, parameter op_l = 3, parameter p = 1)(
+	input [op_lv:0] Opcode,
+    output reg LoadUpperImmediate,
+    output reg [pv:0] ALUOpcode,
+    output reg UseImmediate
+);
+
+parameter lv = l-1;
+parameter op_lv = op_l-1;
+parameter pv = p-1;
+
+always @(Opcode) begin
+	case(Opcode)
+        // MUL
+        3'b111: begin
+            ALUOpcode = 1;
+            UseImmediate = 0;
+            LoadUpperImmediate = 0;
+        end
+        // DIV
+        3'b000: begin
+            ALUOpcode = 0;
+            UseImmediate = 0;
+            LoadUpperImmediate = 0;
+        end
+        // MULi
+        3'b001: begin
+            ALUOpcode = 1;
+            UseImmediate = 1;
+            LoadUpperImmediate = 0;
+        end
+        // DIVi
+        3'b010: begin
+            ALUOpcode = 0;
+            UseImmediate = 1;
+            LoadUpperImmediate = 0;
+        end
+        // LUI
+        3'b011: begin
+            ALUOpcode = 0;
+            UseImmediate = 0;
+            LoadUpperImmediate = 1;
+        end
+	endcase
+end
+	
+endmodule //ControlUnit
---
+++ src/ControlUnit/control_unit_tb.v
@@ -0,0 +1,48 @@
+`include "./control_unit.v"
+
+// This was generated using Copilot
+module ControlUnit_Testbench ();
+
+    parameter l = 5;
+    parameter lv = l-1;
+    parameter p = 0;
+    parameter pv = p-1;
+    parameter op_l = 3;
+    parameter op_lv = op_l-1;
+
+    reg [op_lv:0] Opcode;
+    wire LoadUpperImmediate;
+    wire [pv:0] ALUOpcode;
+    wire UseImmediate;
+
+    ContolUnit #(l, op_l, p) control_unit(
+        .Opcode(Opcode),
+        .LoadUpperImmediate(LoadUpperImmediate),
+        .ALUOpcode(ALUOpcode),
+        .UseImmediate(UseImmediate)
+    );
+
+    initial begin
+        $monitor("LoadUpperImmediate=%d\nALUOpcode=%d\nUseImmediate=%d\n\n", LoadUpperImmediate, ALUOpcode, UseImmediate);
+        
+        $display("MUL:");
+        Opcode = 3'b111;
+        #10;
+
+        $display("DIV:");
+        Opcode = 3'b000;
+        #10;
+        
+        $display("MULi:");
+        Opcode = 3'b001;
+        #10;
+
+        $display("DIVi:");
+        Opcode = 3'b010;
+        #10;
+
+        $display("LUI:");
+        Opcode = 3'b011;
+        #10;
+    end
+endmodule
