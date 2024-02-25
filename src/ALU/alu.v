5f6e3200c795b98455d0688a30768019f09e8137
Roman Mishchuk <ge74jat@mytum.de>
2024-02-25 15:16:25 +0200

    First implementation of ALU

--- src/ALU/alu.v
+++ src/ALU/alu.v
@@ -1,3 +1,7 @@
+`include "./multiplication.v"
+`include "./division.v"
+`include "./inversion.v"
+
 // p - describes the depth of amount of operations
 module ALU #(parameter l=16, parameter p=0) (
 	input [p:0] Operation,
@@ -9,7 +13,50 @@ module ALU #(parameter l=16, parameter p=0) (
 );
 
 parameter lv = l-1;
+// https://stackoverflow.com/a/63209787
+`include "../flags.vh"
 
-// TODO: implement ALU
+wire [lv:0] AbsA;
+AbsoluteValue #(l) abs_a(
+	.A(A), .R(AbsA)
+);
+wire [lv:0] AbsB;
+AbsoluteValue #(l) abs_b(
+	.A(B), .R(AbsB)
+);
+
+wire [lv:0] UnsignedR;
+GiveSign #(l) give_sign(
+	.Sign(A[lv] ^ B[lv]),
+	.A(UnsignedR),
+	.R(R)
+);
+
+always @(*) begin
+	case (Operation)
+		0: begin
+			// Division
+			Division #(l) divider(
+				.A(AbsA), .B(AbsB),
+				.Quotient(UnsignedR),
+				.HasRemainder(FlagsOut[DivisionHasRemainderIdx[lv:0]]),
+				.DivByZero(FlagsOut[DivisionByZeroIdx[lv:0]])
+			);
+			FlagsOut[MultiplicationOverflowIdx[lv:0]] = FlagsIn[MultiplicationOverflowIdx[lv:0]];
+		end
+		1: begin
+			// Multiplication
+			Multiplication #(l) multiplier(
+				.A(AbsA), .B(AbsB),
+				.R1(UnsignedR),
+				.Overflow(FlagsOut[MultiplicationOverflowIdx[lv:0]])
+			);
+			FlagsOut[DivisionHasRemainderIdx[lv:0]] = FlagsIn[DivisionHasRemainderIdx[lv:0]];
+			FlagsOut[DivisionByZeroIdx[lv:0]] = FlagsIn[DivisionByZeroIdx[lv:0]];
+		end
+	endcase
+
+	FlagsOut[lv:ZeroIdx[lv:0]] = FlagsIn[lv:ZeroIdx[lv:0]];
+end
 
 endmodule //ALU
---
+++ src/flags.vh
@@ -0,0 +1,4 @@
+parameter MultiplicationOverflowIdx = 0;
+parameter DivisionHasRemainderIdx = 1;
+parameter DivisionByZeroIdx = 2;
+parameter NoFlagsIdx = 3;
