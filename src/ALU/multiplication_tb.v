a0f123cc24ffd171697c031d01b5deb46697b555
Roman Mishchuk <ge74jat@mytum.de>
2024-03-08 22:33:15 +0200

    Comment regarding mul_tb register size + some adjustments so that it works

--- src/ALU/multiplication_tb.v
+++ src/ALU/multiplication_tb.v
@@ -4,11 +4,13 @@
 // This was generated using Copilot
 module Multiplication_Testbench ();
 
-	reg [15:0] A;
-	reg [15:0] B;
-	wire [15:0] R1;
+	reg [16:0] A;
+	reg [16:0] B;
+	wire [16:0] R1;
 	wire Overflow;
 
+	// Controls the size of the registers (to more easily see the overflow)
+	// Can be set back to 16 to see how the actual module would behave
 	parameter l = 3;
 	Multiplication #(l) multiplication(
 		.A(A[l-1:0]),
@@ -16,7 +18,7 @@ module Multiplication_Testbench ();
 		.R1(R1[l-1:0]),
 		.Overflow(Overflow)
 	);
-	assign R1[15:l] = 0;
+	assign R1[16:l] = 0;
 
 	initial begin
 		$monitor("A=%d,\nB=%d\nR1=%d, O=%b\n", A, B, R1, Overflow);
