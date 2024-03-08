c7d3c4229c0ff77b36ace797134548e92de1f127
Roman Mishchuk <ge74jat@mytum.de>
2024-03-09 01:12:08 +0200

    Addition replaced with XY

--- src/ALU/addition.v
+++ src/ALU/addition.v
@@ -1,19 +1,19 @@
 module Adder (
-	input A,
-	input B,
+	input X,
+	input Y,
 	input Cin,
 	output S,
 	output Cout
 );
 
-assign S = A ^ B ^ Cin;
-assign Cout = (A & B) | ((A ^ B) & Cin);
+assign S = X ^ Y ^ Cin;
+assign Cout = (X & Y) | ((X ^ Y) & Cin);
 
 endmodule //Adder
 
 module FullAdder #(parameter l = 16) (
-	input [lv:0] A,
-	input [lv:0] B,
+	input [lv:0] X,
+	input [lv:0] Y,
 	input Cin,
 	output [lv:0] S,
 	output [lv:0] Cout
@@ -29,7 +29,7 @@ assign Cout_temp[0] = Cin;
 generate genvar i;
 	//                                     |-> Why you do this?
 	for (i = 0; i <= lv; i = i + 1) begin : gen_adders
-		Adder adder(A[i], B[i], Cout_temp[i], Sum[i], Cout_temp[i+1]);
+		Adder adder(X[i], Y[i], Cout_temp[i], Sum[i], Cout_temp[i+1]);
 	end
 endgenerate
 
@@ -48,8 +48,8 @@ In signed arithmetic, the carry flag tells you nothing interesting.
 */
 // This addition algorithm works both for signed and unsigned numbers (2's complement)
 module FullAdderFlags #(parameter l = 16) (
-	input [lv:0] A,
-	input [lv:0] B,
+	input [lv:0] X,
+	input [lv:0] Y,
 	output [lv:0] S,
 	output Overflow,
 	output Carry
@@ -60,7 +60,7 @@ parameter lv = l-1;
 wire [lv:0] Cout;
 
 FullAdder #(l) full_adder (
-	.A(A), .B(B), .Cin(1'b0),
+	.X(X), .Y(Y), .Cin(1'b0),
 	.S(S), .Cout(Cout)
 );
 
