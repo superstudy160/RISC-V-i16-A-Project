154a8d9c524eeac752acf519e745a95c9a8565c4
Roman Mishchuk <ge74jat@mytum.de>
2024-03-09 01:21:27 +0200

    Inversion replaced with XY

--- src/ALU/inversion.v
+++ src/ALU/inversion.v
@@ -9,8 +9,8 @@ Consider --a = -(~a + 1) = ~(~a + 1) + 1 =
 Where M is all ones.
 */
 module Inverter #(parameter l=16) (
-	input [lv:0] A,
-	output [lv:0] R,
+	input [lv:0] Input,
+	output [lv:0] Result,
 	output Overflow
 );
 
@@ -18,10 +18,10 @@ parameter lv = l-1;
 
 wire ignore;
 FullAdderFlags #(l) full_adder_flags(
-	.A(~A),
-	.B({{l-1{1'b0}}, 1'b1}),
+	.X(~Input),
+	.Y({{l-1{1'b0}}, 1'b1}),
 
-	.S(R),
+	.S(Result),
 	.Overflow(Overflow), // This would correspond to not changing the sign of A
 	.Carry(ignore)
 );
@@ -31,8 +31,8 @@ endmodule
 
 // Given input of size 4, the maximum output is 1000
 module AbsoluteValue #(parameter l=16) (
-	input [lv:0] A,
-	output [lv:0] R
+	input [lv:0] Input,
+	output [lv:0] Result
 );
 
 parameter lv = l-1;
@@ -40,19 +40,19 @@ parameter lv = l-1;
 wire [lv:0] inv;
 wire ignore;
 Inverter #(l) inverter(
-	.A(A), .R(inv),
+	.Input(Input), .Result(inv),
 	.Overflow(ignore)
 );
 
-assign R = A[lv] ? inv : A;
+assign Result = Input[lv] ? inv : Input;
 
 endmodule
 
 
 module GiveSign #(parameter l=16) (
 	input Sign, // 1 for negative, 0 for positive
-	input [lv:0] A,
-	output [lv:0] R,
+	input [lv:0] Input,
+	output [lv:0] Result,
 	output Overflow
 );
 
@@ -61,11 +61,11 @@ parameter lv = l-1;
 wire [lv:0] inv;
 wire ignore;
 Inverter #(l) inverter(
-	.A(A), .R(inv),
+	.Input(Input), .Result(inv),
 	.Overflow(ignore)
 );
 
-assign R = Sign ? inv : A;
-assign Overflow = !Sign & A == {1'b1, {l-1{1'b0}}}; // E.g., if l=4, there is no way to represenet +8 in 2's complement
+assign Result = Sign ? inv : Input;
+assign Overflow = !Sign & Input == {1'b1, {l-1{1'b0}}}; // E.g., if l=4, there is no way to represenet +8 in 2's complement
 
 endmodule
--- src/ALU/inversion_tb.v
+++ src/ALU/inversion_tb.v
@@ -4,18 +4,18 @@
 module Inverter_Testbench ();
 
 parameter l = 4;
-reg signed [l-1:0] A;
+reg signed [l-1:0] Input;
 
 wire signed [l-1:0] R_inv;
 wire Overflow_inv;
 Inverter #(l) inverter(
-    .A(A), .R(R_inv),
+    .Input(Input), .Result(R_inv),
     .Overflow(Overflow_inv)
 );
 
 wire [l-1:0] R_abs;
 AbsoluteValue #(l) absolute_value(
-    .A(A), .R(R_abs)
+    .Input(Input), .Result(R_abs)
 );
 
 reg Sign;
@@ -24,35 +24,35 @@ wire signed [l-1:0] R_sign;
 wire Overflow_sign;
 GiveSign #(l) give_sign(
     .Sign(Sign),
-    .A(A_sign), .R(R_sign),
+    .Input(A_sign), .Result(R_sign),
     .Overflow(Overflow_sign)
 );
 
 initial begin
     $display("Testing inverter:");
-    $monitor("A=%d\nR=%d  O=%b\n", A, R_inv, Overflow_inv);
-    A = -4'sd1;
+    $monitor("Input=%d\nResult=%d  O=%b\n", Input, R_inv, Overflow_inv);
+    Input = -4'sd1;
     #10;
-    A = -4'sd8;
+    Input = -4'sd8;
     #10;
-    A = 4'sd7;
+    Input = 4'sd7;
     #10;
-    A = 4'sd5;
+    Input = 4'sd5;
     #10;
 
     $display("Testing absolute value:");
-    $monitor("A=%d R=%d\n", A, R_abs);
-    A = -4'sd1;
+    $monitor("Input=%d Result=%d\n", Input, R_abs);
+    Input = -4'sd1;
     #10;
-    A = -4'sd8;
+    Input = -4'sd8;
     #10;
-    A = 4'sd7;
+    Input = 4'sd7;
     #10;
-    A = -4'sd5;
+    Input = -4'sd5;
     #10;
 
     $display("Testing sign setting:");
-    $monitor("Sign=%d\nA=%d R=%d  O=%b\n", Sign, A_sign, R_sign, Overflow_sign);
+    $monitor("Sign=%d\nInput=%d Result=%d  O=%b\n", Sign, A_sign, R_sign, Overflow_sign);
     Sign = 1'b0;
     A_sign = 4'd5;
     #10;
